ee0ff1fcca5adcbf973cdac1ab35dbfa3f41e55e
Israel P. Coelho <ipcoelho.business@gmail.com>
2024-09-29 16:00:52 -0300

    feat: Renamed main file to the application name

---
+++ src/main/java/br/com/hinova/powerpdf/PowerPdfApplication.java
@@ -0,0 +1,12 @@
+package br.com.hinova.powerpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerPdfApplication.class, args);
+    }
+}
